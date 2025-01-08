b38a7f8a2f8344aaa93f8eaddf0ff029ce58475f
NicolasRaony06 <nicolas.raony06@gmail.com>
2025-01-08 13:30:13 -0300

    Add tabelas Person e Journal; pagina escrever

--- diario_online/journal/models.py
+++ diario_online/journal/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    foto = models.ImageField(upload_to='fotos')
+
+    def __str__(self):
+        return self.name
+    
+class Journal(models.Model):
+    tittle = models.CharField(max_length=100)
+    tags = models.TextField()
+    text = models.TextField()
+    person = models.ManyToManyField(Person, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tittle
--- diario_online/journal/views.py
+++ diario_online/journal/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 def home(request):
     if request.method == 'GET':
         return render(request, 'home.html')
+    
+def write(request):
+    if request.method == 'GET':
+        return render(request, 'write.html')
+    
+    if request.method == 'POST':
+        tittle = request.POST.get('titulo')
+        tags = request.POST.getlist('tags')
+        person = request.POST.getlist('pessoas')
+        text = request.POST.get('texto')
+
+        print(tittle, tags, person, text)
+
+        return redirect(write)
